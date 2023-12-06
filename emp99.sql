4250a56d6c18362345f9ae975a4e37e4bcf6ce62
meraj7408 <108111906+meraj7408@users.noreply.github.com>
2023-12-06 12:04:48 +0530

    @Meraj Alam
    Bean Class:- the bean class contains the variables (along setter and getter methods) corresponding to the fields exist in the database.
    DAO CLASS:-DAO class to access the required data from the database.

---
+++ emp99.sql
@@ -0,0 +1,9 @@
++-------------+-------------+------+-----+---------+----------------+
+| Field       | Type        | Null | Key | Default | Extra          |
++-------------+-------------+------+-----+---------+----------------+
+| id          | int         | NO   | PRI | NULL    | auto_increment |
+| Name        | varchar(45) | YES  |     | NULL    |                |
+| Salary      | int         | YES  |     | NULL    |                |
+| Designation | varchar(45) | YES  |     | NULL    |                |
++-------------+-------------+------+-----+---------+----------------+
+4 rows in set (0.00 sec)
